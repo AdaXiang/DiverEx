25a045913ee039832c39c3b7c0bda14e769e4b6b
MSG8 <masogo008@gmail.com>
2026-04-21 18:13:39 +0200

    version 1 de backend, con entorno virtual

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,24 @@
-from dotenv import load_dotenv
-import os
+from fastapi import FastAPI #Api para operaciones backend
+from dotenv import load_dotenv #Seguridad de variables de entorno
+import os #Trabajar con rutas 
 
+#Variables entorno
 load_dotenv()
+COUCHDB_URL = os.getenv("COUCHDB_URL") #Prueba de que va bien las variables de entorno
 
-COUCHDB_URL = os.getenv("COUCHDB_URL")
+#Inicializar la api
+app = FastAPI()
+# Endpoint básico
+@app.get("/")
+def read_root():
+    return {
+        "message": "Bienvenido a la API de Badajoz GeoApp 🚀"
+    }
+
+# Endpoint de prueba
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok",
+        "service": "backend running"
+    }
